c582a7bff8fda267f54be5d710bbac1ffa4a149e
Eleazar Villarreal <EleazarVillarreal13@gmail.com>
2024-12-04 21:50:00 -0600

    added logo and favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,7 @@ export default function Home() {
   return (
     <main className="mx-auto min-h-screen max-w-6xl bg-white drop-shadow-sm">
       <div className="mx-auto min-h-screen max-w-3xl px-6">
-        <h2 className="mb-4 text-xl font-bold text-zinc-800 md:text-2xl">
-          Locations
-        </h2>
+        <h2 className="text-zinc-800l mb-4 text-2xl font-bold">Locations</h2>
       </div>
     </main>
   )
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,13 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 export function Header() {
   return (
     <header className="mx-auto max-w-6xl bg-white drop-shadow-sm">
-      <div className="mx-auto max-w-3xl p-6">
+      <div className="mx-auto flex max-w-3xl items-center p-6">
+        <Image alt="" className="mr-2" height={36} src="/beer.png" width={36} />
         <Link href="/">
-          <h1 className="text-2xl font-bold text-zinc-800 md:text-4xl">
-            ATX Social Hour
-          </h1>
+          <h1 className="text-3xl font-bold text-zinc-800">ATX Social Hour</h1>
         </Link>
       </div>
     </header>
